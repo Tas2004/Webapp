bce3a510012c33e84706c939b23502aeaa3ce7e4
Tas2004 <tasneemwamut2004@gmail.com>
2026-01-08 15:18:46 +0700

    Update model_dumb.py

--- Backend/model_dumb.py
+++ Backend/model_dumb.py
@@ -1,11 +1,11 @@
 import joblib
-import xgboost as xgb
+import RandomForestClassifier as rf
 
 model_dumb = joblib.load(rf"D:\Thermal_comfort\train_test_Model\2Class\Set6\rf_model.pkl")
 
 #"D:\Work\Project\Model_Dumb\Model_Dumbxgb_model_['EDA_Phasic_EmotiBit', 'EDA_Tonic_EmotiBit', 'BMI', 'SkinTemp_Emo'].pkl"
 
-if isinstance(model_dumb, xgb.XGBClassifier):
-    print("XGBoost model loaded successfully!")
+if isinstance(model_dumb, rf.RandomForestClassifier):
+    print("RandomForest model loaded successfully!")
 else:
-    raise ValueError("Loaded model is not an XGBClassifier. Please check the file path.")
+    raise ValueError("Loaded model is not an RandomForestClassifier. Please check the file path.")
