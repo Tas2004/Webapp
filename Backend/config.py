2acee2d7cae949a86a406eb3d2b2d9000c54ba3e
Tas2004 <tasneemwamut2004@gmail.com>
2026-01-09 18:09:16 +0700

    Update config.py

--- Backend/config.py
+++ Backend/config.py
@@ -2,7 +2,9 @@ import firebase_admin
 from firebase_admin import credentials
 
 def initialize_firebase():
-    cred = credentials.Certificate("./data-1e66c-firebase-adminsdk-fbsvc-042297bdbe.json")
+    cred = credentials.Certificate(
+        r"D:\Webapp-main\Backend\thermalcomfort-655cf-firebase-adminsdk-fbsvc-2ce2d6c237.json"
+    )
     firebase_admin.initialize_app(cred, {
-        "databaseURL": "https://data-1e66c-default-rtdb.asia-southeast1.firebasedatabase.app/"
+        "databaseURL": r"https://thermalcomfort-655cf-default-rtdb.asia-southeast1.firebasedatabase.app/"
     })
